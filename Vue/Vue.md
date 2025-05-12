765ed1e38d11d95fe1b5d2a9fe456b185202dfcd
benson1231 <benson106075@gmail.com>
2025-05-12 23:12:29 +0800

    update Vue

---
+++ Vue/Vue.md
@@ -0,0 +1,16 @@
+# Download vue
+```bash
+npm init vue@latest
+```
+# Download package
+```bash
+npm install
+```
+# 開發測試 development
+```bash
+npm run dev
+```
+# 專案建置 production
+```bash
+npm run build
+```
--- Vue/vue-training/src/App.vue
+++ Vue/vue-training/src/App.vue
@@ -1,59 +1,40 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+console.log("Hello world");
+let message = "Hello vue";
+let word = "<u>important</u>";
+let name = "content";
+let checked = false;
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
-  <RouterView />
+  <main>
+    <p>{{ message }}</p>
+    <p>{{ message.toUpperCase() }}</p>
+    <p v-html="word"></p>
+    <div :class="name">apple</div>
+    <p :class="checked?'':'active'">apple</p>
+    <TheWelcome />
+  </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-  max-height: 100vh;
 }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
 }
 
 @media (min-width: 1024px) {
@@ -67,19 +48,18 @@ nav a:first-of-type {
     margin: 0 2rem 0 0;
   }
 
+  .content {
+    color: red;
+  }
+
+  .active {
+    color: green;
+  }
+
   header .wrapper {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
